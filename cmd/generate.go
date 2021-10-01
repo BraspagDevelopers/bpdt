10c4d77e56c0edd26b5fe877db50125d0d1834f0
Andre Soares <andregsilv@gmail.com>
2021-10-01 15:41:55 -0300

    generate configmap

---
+++ cmd/generate.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/BraspagDevelopers/bpdt/lib"
+	"github.com/spf13/cobra"
+)
+
+var generateCmd = &cobra.Command{
+	Use:     "generate",
+	Aliases: []string{"gen"},
+}
+
+var generateConfigMapCmd = &cobra.Command{
+	Use:   "configmap",
+	Short: "Generate a ConfigMap manifest",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		name := args[0]
+
+		fromEnv, err := cmd.Flags().GetBool("env")
+		handleError(err)
+
+		prefix, err := cmd.Flags().GetString("prefix")
+		handleError(err)
+
+		keepPrefix, err := cmd.Flags().GetBool("env")
+		handleError(err)
+
+		configMap, err := lib.GenerateConfigMap(lib.GenerateConfigMapParams{
+			Name:            name,
+			FromEnvironment: fromEnv,
+			Prefix:          prefix,
+			KeepPrefix:      keepPrefix,
+		})
+		handleError(err)
+
+		fmt.Println(configMap)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(generateCmd)
+	generateCmd.AddCommand(generateConfigMapCmd)
+
+	generateConfigMapCmd.Flags().Bool("env", false, "Load variables from environment")
+	generateConfigMapCmd.Flags().Bool("keep-prefix", false, "Keep the variable name prefix")
+
+	generateConfigMapCmd.Flags().String("prefix", "", "Filter the variables by this prefix")
+}
---
+++ lib/generate.go
@@ -0,0 +1,86 @@
+package lib
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+type GenerateConfigMapParams struct {
+	Name            string
+	FromEnvironment bool
+	Prefix          string
+	KeepPrefix      bool
+}
+
+func (p GenerateConfigMapParams) Validate() error {
+	if !p.FromEnvironment {
+		return errors.New("environment is the only source for now")
+	}
+}
+
+func (p GenerateConfigMapParams) getVariables() (map[string]string, error) {
+	envLines := os.Environ()
+	result := make(map[string]string, 0)
+	for n, envLine := range envLines {
+		splitted := strings.SplitN(envLine, "=", 2)
+		if len(splitted) != 2 {
+			return nil, fmt.Errorf("the %dth environment variable and value could not be parsed", n)
+		}
+
+		key := splitted[0]
+		value := splitted[1]
+
+		if p.isValidKey(key) {
+			key = p.normalizeKey(key)
+			result[key] = value
+		}
+	}
+
+	return result, nil
+}
+
+func (p GenerateConfigMapParams) isValidKey(key string) bool {
+	return strings.HasPrefix(key, p.Prefix)
+}
+
+func (p GenerateConfigMapParams) normalizeKey(key string) string {
+	if !p.KeepPrefix && len(p.Prefix) > 0 {
+		key = key[len(p.Prefix):]
+	}
+	return key
+}
+
+func (p GenerateConfigMapParams) marshalConfigMap(dataMap map[string]string) (string, error) {
+	manifest := map[string]interface{}{
+		"apiVersion": "v1",
+		"kind":       "ConfigMap",
+		"metadata": map[string]string{
+			"name": p.Name,
+		},
+		"data": dataMap,
+	}
+	data, err := yaml.Marshal(manifest)
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
+
+func GenerateConfigMap(params GenerateConfigMapParams) (string, error) {
+	err := params.Validate()
+	if err != nil {
+		return "", err
+	}
+
+	envVars, err := params.getVariables()
+	if err != nil {
+		return "", err
+	}
+
+	return params.marshalConfigMap(envVars)
+}
